d8d4383edbf7fe84459aae7946218700f6f89d52
@tommyRequillard <tommy.requillard@gmail.com>
2025-03-16 20:04:25 +0100

    vite config update

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,15 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [tailwindcss(),],
+  build: {
+    chunkSizeWarningLimit: 600,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          // Regrouper les dépendances volumineuses dans un chunk séparé
+          vendor: ['react', 'react-dom', 'papaparse', 'xlsx'],
+        },
+      },
+    },
+  },
 })
